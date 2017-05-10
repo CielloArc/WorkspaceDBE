aee62e010d3a467c1e87b9fc26ee23d6997e83b2
logonrm <logonrm@fiap.com.br>
2017-05-10 08:02:13 -0300

    Criacao da Workspace DBE

---
+++ 2TDSA-Digital-master.zip_expanded/2TDSA-Digital-master/18-JSF-JPA/src/br/com/fiap/bean/ListaConsultorBean.java
@@ -0,0 +1,52 @@
+package br.com.fiap.bean;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import br.com.fiap.bo.ConsultorBO;
+import br.com.fiap.entity.Consultor;
+import br.com.fiap.exception.DBException;
+
+@ManagedBean
+public class ListaConsultorBean {
+
+	private ConsultorBO bo;
+	
+	private List<Consultor> lista;
+	
+	@PostConstruct
+	private void init(){
+		bo = new ConsultorBO();
+		lista = bo.listar();
+	}
+	
+	public String excluir(int codigo){
+		FacesMessage msg;
+		try {
+			bo.remover(codigo);
+			msg = new FacesMessage("Consultor excluido!");
+		} catch (DBException e) {
+			e.printStackTrace();
+			msg = new FacesMessage("Erro");
+		}
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		//Manter a mensagem após o redirect
+		FacesContext.getCurrentInstance().getExternalContext()
+										.getFlash().setKeepMessages(true);
+		//nome do arquivo ? redirect
+		return "lista-consultor?faces-redirect=true";
+	}
+
+	public List<Consultor> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Consultor> lista) {
+		this.lista = lista;
+	}
+	
+}
